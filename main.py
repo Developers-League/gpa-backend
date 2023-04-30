9c2a61dd5bc977112f359cba3354fec10886daab
Adehwam21 <drewaaron96@gmail.com>
2023-04-30 03:40:24 +0000

    added endpoint paths

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from base import grade_to_classification, main_grades, getList
 
 
 #  API class
@@ -24,18 +25,29 @@ Endpoints
 
 1. calc-fgpa
 2. calc-gpa-and-cgpa
-3. calc-required-grades-for-sgpa
-4. calc-highest-reachable-gpa-per-sem
-5. calc-lowest-reachable-gpa-per-sem
+3. calc-req-grades-for-rgpa
+4. calc-min-max-gpa-per-sem
 
 '''
+# FGPA calculation endpoint
+@app.post('/api/calc-fgpa')
+async def fgpa(cgpa1, cgpa2, cgpa3, cgpa4):
+    pass
 
+# GPA and CGPA calculation endpoint
+@app.post('/api/calc-gpa-and-cgpa')
+async def new_gpa_calc(grades, credit):
+    pass
 
-@app.get('/api/')
-async def root():
-    a = 2
-    b = 5
-    c = a + b
-    return {c}
+# Required grades calculation endpoint
+@app.post('/api/calc-req-grades-for-sgpa')
+async def calc_gpa_needed(old_cgpa, new_cgpa, old_chours, new_chours):
+    pass
+
+# Minimum and Maximum GPA calculation endpoint
+@app.post('/api/calc-min-max-gpa-per-sem')
+async def min_max_cgpa(old_chours, new_chours, old_cgpa):
+    pass
+
 
 

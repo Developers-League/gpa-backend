186f6df20ad73f906027fd312ce741ff917a7029
Adehwam21 <drewaaron96@gmail.com>
2023-05-02 04:30:09 +0000

    minor fixes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from base import grade_to_classification, main_grades, getList
+
 
 
 #  API class
@@ -20,34 +20,22 @@ app.add_middleware(
 
 # Default function for the API default path
 
-'''
-Endpoints 
-
-1. calc-fgpa
-2. calc-gpa-and-cgpa
-3. calc-req-grades-for-rgpa
-4. calc-min-max-gpa-per-sem
-
-'''
-# FGPA calculation endpoint
-@app.post('/api/calc-fgpa')
-async def fgpa(cgpa1, cgpa2, cgpa3, cgpa4):
-    pass
-
-# GPA and CGPA calculation endpoint
-@app.post('/api/calc-gpa-and-cgpa')
-async def new_gpa_calc(grades, credit):
-    pass
-
-# Required grades calculation endpoint
-@app.post('/api/calc-req-grades-for-sgpa')
-async def calc_gpa_needed(old_cgpa, new_cgpa, old_chours, new_chours):
-    pass
-
-# Minimum and Maximum GPA calculation endpoint
-@app.post('/api/calc-min-max-gpa-per-sem')
-async def min_max_cgpa(old_chours, new_chours, old_cgpa):
-    pass
+@app.get('/')
+async def root():
+    return {"ping": "pong"}
 
 
+# # FGPA calculation endpoint
+# @app.post('/api/calc-fgpa')
 
+
+# # GPA and CGPA calculation endpoint
+# @app.post('/api/calc-gpa-and-cgpa')
+
+
+# # Required grades calculation endpoint
+# @app.post('/api/calc-req-grades-for-sgpa')
+
+
+# # Minimum and Maximum GPA calculation endpoint
+# @app.post('/api/calc-min-max-gpa-per-sem')
--- model.py
+++ model.py
@@ -2,16 +2,25 @@ from pydantic import BaseModel
 
 # FGPA form data
 class fgpa(BaseModel):
-    pass
+    cgpa1: float
+    cgpa2: float
+    cgpa3: float
+    cgpa4: float
 
 # CGPA form data
 class cgpa(BaseModel):
-    pass
+    grades: str
+    credit: list 
 
 # Minimum and Maximum CGPA form data
 class min_max_cgpa(BaseModel):
-    pass
+    old_chours: int
+    new_chours: int
+    old_cgpa: float
 
 # Required grades to hit a desired CGPA form data
 class required_cgpa(BaseModel):
-    pass
+    old_cgpa : float
+    new_cgpa : float
+    old_chours: int
+    new_chours: int

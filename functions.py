8bad8ae55408242bff62943a993b82aad9c5ae46
Adehwam21 <drewaaron96@gmail.com>
2023-05-02 04:26:46 +0000

    working on Fgpa functions

---
+++ functions.py
@@ -0,0 +1,15 @@
+from base import *
+from model import *
+
+
+async def fgpa(cgpa1, cgpa2, cgpa3, cgpa4):
+    pass
+
+async def new_gpa_calc(grades, credit):
+    pass
+
+async def calc_gpa_needed(old_cgpa, new_cgpa, old_chours, new_chours):
+    pass
+
+async def min_max_cgpa(old_chours, new_chours, old_cgpa):
+    pass
